9e29a32ece2fe1dbd105b21ad632cf9de31d6bdd
EC2 Default User <ec2-user@ip-172-31-18-30.ec2.internal>
2022-06-04 11:26:39 +0000

    finish

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < ApplicationController
+  def index
+    @user = User.find(current_user.id)
+    @book = Book.new
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
+  end
+  
+  def create
+    @user = User.find(current_user.id)
+    @books = Book.all
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'You have created book successfully.'
+    else
+      render :index
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user.id == current_user.id
+      render "edit"
+    else
+      redirect_to user_path(current_user.id)
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: 'You have updated user successfully.'
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'a06f1810e3885e42bcb6cfbaf43d4b8fc273ef8094d4a33807ca4c11d94e1aed9f86e11d08feaf3420488fd7e40224ea3cb3707e768d06139a6baa6d3d662784'
+
